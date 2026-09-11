d7a142456bc094e721fb612d5a020e69fb87a2f8
an7ln <280216zyw@gmail.com>
2026-09-10 22:37:43 -0700

    fix: serve lowercase utf8 readme notice (#12)
    Co-authored-by: Aining777 <2802160236@qq.com>

--- src/pages/readme.txt.ts
+++ src/pages/readme.txt.ts
@@ -1,4 +1,4 @@
-import content from '../../public/README.txt?raw';
+import content from '../../public/readme.txt?raw';
 
 export const GET = () => new Response(content, {
   headers: { 'Content-Type': 'text/plain; charset=utf-8' },
